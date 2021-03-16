87bb10481f2bca3d76f4dfbfcecf80c94102e0dc
sambhav25-NITK <sambhavkhandelwal25@gmail.com>
2021-03-16 11:26:54 +0530

    first commit

---
+++ src/main/java/com/spring/swagger/demoswagger/AddressBookResource.java
@@ -0,0 +1,35 @@
+package com.spring.swagger.demoswagger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AddressBookResource {
+	
+	HashMap<String,Contact> contacts=new HashMap<>();
+
+	@GetMapping("/{id}")
+	public Contact getContact(@PathVariable String id)
+	{
+		return contacts.get(id);
+	}
+	@GetMapping("/")
+	public List<Contact> getAllContact(){
+		
+		return new ArrayList<Contact>(contacts.values());
+	}
+	@PostMapping("/")
+	public Contact addContact(@RequestBody Contact contact)
+	{
+		contacts.put(contact.getId(), contact);
+		return contact;
+	}
+}
---
+++ src/main/java/com/spring/swagger/demoswagger/DemoSwaggerApplication.java
@@ -0,0 +1,22 @@
+package com.spring.swagger.demoswagger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class DemoSwaggerApplication {
+
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSwaggerApplication.class, args);
+	}
+
+	
+
+}
