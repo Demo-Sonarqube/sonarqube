6962dc6a714d3a3a1d63855ace5d7cdac975f66d
sambhav25-NITK <65706477+sambhav25-NITK@users.noreply.github.com>
2021-03-27 10:13:57 +0530

    Update First.java

--- src/main/java/com/spring/swagger/demoswagger/First.java
+++ src/main/java/com/spring/swagger/demoswagger/First.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hello vishal this file is created for demo scm ::  ");
+        
       HashMap<String,String> m1=new HashMap<String,String>();
       m1.put("Vishal","patidar");
       
