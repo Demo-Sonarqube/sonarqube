f0951a8599acbc1d5811c23c2d13d478b9257037
vishal patidar <44113789+patidar622@users.noreply.github.com>
2021-03-27 10:02:50 +0530

    new file by vishal for scm demo

---
+++ src/main/java/com/spring/swagger/demoswagger/First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello vishal this file is created for demo scm ::  ");
+      HashMap<String,String> m1=new HashMap<String,String>();
+      m1.put("Vishal","patidar");
+      
+    }
+}
