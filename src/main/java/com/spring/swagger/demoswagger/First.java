751a564b37c001b513d2dae31b6b7eca72c60000
sambhav25-NITK <65706477+sambhav25-NITK@users.noreply.github.com>
2021-03-28 11:02:57 +0530

    Delete First.java

--- src/main/java/com/spring/swagger/demoswagger/First.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-public class First {
-    public static void main(String[] args) {
-        
-      HashMap<String,String> m1=new HashMap<String,String>();
-      m1.put("Vishal","patidar");
-      
-    }
-}
