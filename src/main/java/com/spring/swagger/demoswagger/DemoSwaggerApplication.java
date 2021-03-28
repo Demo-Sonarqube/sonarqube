ecd8c14e711bba018c8c84f64dc6cfb860b61f50
sambhav25-NITK <65706477+sambhav25-NITK@users.noreply.github.com>
2021-03-28 13:53:48 +0530

    Update DemoSwaggerApplication.java

--- src/main/java/com/spring/swagger/demoswagger/DemoSwaggerApplication.java
+++ src/main/java/com/spring/swagger/demoswagger/DemoSwaggerApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import java.util.Date;
-import java.awt.*;
+
 
 @SpringBootApplication
 @EnableSwagger2
