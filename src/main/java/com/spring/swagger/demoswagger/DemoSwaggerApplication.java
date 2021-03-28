6ca77e4c3c2d02f17a84f340accd61b3a5c1d21f
sambhav25-NITK <65706477+sambhav25-NITK@users.noreply.github.com>
2021-03-28 11:21:25 +0530

    Update DemoSwaggerApplication.java

--- src/main/java/com/spring/swagger/demoswagger/DemoSwaggerApplication.java
+++ src/main/java/com/spring/swagger/demoswagger/DemoSwaggerApplication.java
@@ -14,6 +14,8 @@ public class DemoSwaggerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSwaggerApplication.class, args);
 		System.out.println("sambhav");
+		System.out.println("khandelwal");
+
 
 	}
 
