ab402e3217f4d3f47922c8e94753326a25a8b577
sambhav25-NITK <65706477+sambhav25-NITK@users.noreply.github.com>
2021-03-29 15:14:15 +0530

    Update AddressBookResource.java

--- src/main/java/com/spring/swagger/demoswagger/AddressBookResource.java
+++ src/main/java/com/spring/swagger/demoswagger/AddressBookResource.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 
 
-import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
