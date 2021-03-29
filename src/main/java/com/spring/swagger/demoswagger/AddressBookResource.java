83ce397b000acad1baec2ee0863ef175258228cb
sambhav25-NITK <65706477+sambhav25-NITK@users.noreply.github.com>
2021-03-29 10:49:20 +0530

    Update AddressBookResource.java

--- src/main/java/com/spring/swagger/demoswagger/AddressBookResource.java
+++ src/main/java/com/spring/swagger/demoswagger/AddressBookResource.java
@@ -24,7 +24,7 @@ public class AddressBookResource {
 	}
 	@GetMapping("/")
 	public List<Contact> getAllContact(){
-		
+		System.out.println("TEST4");
 		return new ArrayList<>(contacts.values());
 	}
 	@PostMapping("/")
