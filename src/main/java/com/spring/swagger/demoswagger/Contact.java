217fccc313887539ba8e649e04eee90ebc14eb9b
sambhav25-NITK <65706477+sambhav25-NITK@users.noreply.github.com>
2021-03-31 14:49:51 +0530

    Update Contact.java

--- src/main/java/com/spring/swagger/demoswagger/Contact.java
+++ src/main/java/com/spring/swagger/demoswagger/Contact.java
@@ -1,6 +1,4 @@
 package com.spring.swagger.demoswagger;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Component;
 
 @Component
