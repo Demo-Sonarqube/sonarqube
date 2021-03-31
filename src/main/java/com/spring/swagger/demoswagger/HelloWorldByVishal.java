22c59d5d02951a985b17ff15d2586b1b424791b1
vishal patidar <44113789+patidar622@users.noreply.github.com>
2021-03-31 18:14:50 +0530

    change by vishal adding method

--- src/main/java/com/spring/swagger/demoswagger/HelloWorldByVishal.java
+++ src/main/java/com/spring/swagger/demoswagger/HelloWorldByVishal.java
@@ -1,5 +1,9 @@
 class HelloWorldByVishal {
-    public static void main(String[] args) {
+    public static void main1() {
         System.out.println("Hello, World! vishal"); 
+    }
+    public void vish()
+    {
+        
     }
 }
