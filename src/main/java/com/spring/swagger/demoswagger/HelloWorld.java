c10f0dade88cdc7614a7bd7febd8c342e5cbcb61
sambhav25-NITK <65706477+sambhav25-NITK@users.noreply.github.com>
2021-04-01 09:24:39 +0530

    Update HelloWorld.java

--- src/main/java/com/spring/swagger/demoswagger/HelloWorld.java
+++ src/main/java/com/spring/swagger/demoswagger/HelloWorld.java
@@ -1,13 +1,4 @@
 
-
-import java.util.Map;
-import java.util.List;
-import java.util.HashMap;
-import java.util.Collection;
-import java.util.Deque;
-import java.util.ArrayList;
-import java.util.Vector;
-
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
