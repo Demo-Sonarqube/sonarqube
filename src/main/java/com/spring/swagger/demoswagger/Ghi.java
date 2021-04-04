4c220dacd020714461257bb3bd5f1554ad73d7ee
sambhav25-NITK <65706477+sambhav25-NITK@users.noreply.github.com>
2021-04-04 13:21:13 +0530

    Update Ghi.java

--- src/main/java/com/spring/swagger/demoswagger/Ghi.java
+++ src/main/java/com/spring/swagger/demoswagger/Ghi.java
@@ -1,8 +1,16 @@
 import java.util.Date;
 import java.util.Map;
+import java.util.HashMap;
 class Ghi{
   public void abc()
   {
     System.out.print("sambhav");
+  }
+  public void gh()
+  {
+  }
+  public void cd()
+  {
+    System.out.println("sambhav");
   }
 }
