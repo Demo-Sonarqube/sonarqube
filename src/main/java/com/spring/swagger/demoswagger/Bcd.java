37e072b00a74e96478e734a3859720c1640a6fc9
sambhav25-NITK <65706477+sambhav25-NITK@users.noreply.github.com>
2021-04-04 11:25:47 +0530

    Update Bcd.java

--- src/main/java/com/spring/swagger/demoswagger/Bcd.java
+++ src/main/java/com/spring/swagger/demoswagger/Bcd.java
@@ -1,14 +1,7 @@
-import java.util.HashMap;
-import java.util.Date;
 class Bcd{
   
-  public void sambhav()
-  {
-  
-  }
   public void abc()
   {
-    int b;
     System.out.println("sambhav");
   }
 }
