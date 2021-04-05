a8554c8faee8af815f88e9896ee631097faed288
sambhav25-NITK <sambhavkhandelwal7@gmail.com>
2021-04-05 11:49:38 +0530

    Update Hello.java

--- src/main/java/com/spring/swagger/demoswagger/Hello.java
+++ src/main/java/com/spring/swagger/demoswagger/Hello.java
@@ -1,5 +1,14 @@
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 class Hello {
     public  void main1() {
        System.out.println("Hello vishal");
+    }
+    public void ab()
+    {}
+    public void cd()
+    {
+   System.out.println("a"); 
     }
 }
