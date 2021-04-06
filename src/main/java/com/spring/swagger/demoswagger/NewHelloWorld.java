4c23871986bbdfda575f860b21c53675bba28ee6
sambhav25-NITK <sambhavkhandelwal7@gmail.com>
2021-04-06 11:36:19 +0530

    Update NewHelloWorld.java

--- src/main/java/com/spring/swagger/demoswagger/NewHelloWorld.java
+++ src/main/java/com/spring/swagger/demoswagger/NewHelloWorld.java
@@ -1,5 +1,15 @@
+import java.util.Date;
+import java.util.Map;
+import java.util.Vector;
 class NewHelloWorld {
     public static void main2() {
         System.out.println("Hello, World! by vishal patidar"); 
+        void abc()
+        {
+        }
+        void cd()
+        {
+            System.out.println("sambhav");
+        }
     }
 }
