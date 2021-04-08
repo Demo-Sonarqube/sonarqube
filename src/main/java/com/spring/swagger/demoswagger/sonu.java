6cde6d912758dbf10f2c10b6db583e2f3287953a
sambhav25-NITK <sambhavkhandelwal7@gmail.com>
2021-04-08 12:12:03 +0530

    Update sonu.java

--- src/main/java/com/spring/swagger/demoswagger/sonu.java
+++ src/main/java/com/spring/swagger/demoswagger/sonu.java
@@ -3,5 +3,7 @@ import java.util.HashMap;
 class sonu {
     public void hellosonu() {
         System.out.println( "Hello Sonu!" );
+        Map<String,String> mp=new HashMap<>();
+        mp.put("abc","cd");
     }
 }
